dfa49aea79036e551e8393f4be8cdc22b73671e4
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-02-16 19:42:01 +0100

    Add unity config

--- Borderlands2GoldendKeys/Borderlands2GoldendKeys/Global.asax.cs
+++ Borderlands2GoldendKeys/Borderlands2GoldendKeys/Global.asax.cs
@@ -13,6 +13,7 @@ namespace Borderlands2GoldendKeys
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
