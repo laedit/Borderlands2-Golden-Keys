94bbc8b6317663b458034b630eb55b8415e6c18d
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-26 07:56:38 +0100

    Updated all Nuget packages

--- Borderlands2GoldendKeys/Borderlands2GoldendKeys/Scripts/_references.js
+++ Borderlands2GoldendKeys/Borderlands2GoldendKeys/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="modernizr-2.7.1.js" />
+﻿/// <reference path="modernizr-2.7.2.js" />
+/// <autosync enabled="true" />
 /// <reference path="jquery-2.1.0.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
