474a0d648e5f08261884f31aff089de533fd6420
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-31 07:59:40 +0200

    Added javascript optimization

--- Borderlands2GoldenKeys/Borderlands2GoldenKeys/App_Start/BundleConfig.cs
+++ Borderlands2GoldenKeys/Borderlands2GoldenKeys/App_Start/BundleConfig.cs
@@ -9,21 +9,17 @@ namespace Borderlands2GoldenKeys
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.js").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js",
                         "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/site").Include(
-                      "~/Scripts/site.js"));
+                      "~/Scripts/site.js",
+                      "~/Scripts/jquery.zclip.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/ga").Include(
                       "~/Scripts/ga.js"));
