f00b3b6003865a46f4a6a000ccd5fbef85aaa371
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-04-03 07:41:30 +0200

    Added tooltip on copy to clipboard buttons

--- Borderlands2GoldenKeys/Borderlands2GoldenKeys/Scripts/site.js
+++ Borderlands2GoldenKeys/Borderlands2GoldenKeys/Scripts/site.js
@@ -153,6 +153,7 @@ function InitializezClip()
             $($(this).data('img')).attr("src", "/Content/images/clipboard-ok.png");
         }
     });
+    $('.copyContainer').tooltip();
 }
 
 !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
